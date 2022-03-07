cc12b7d4f46de4146a0c3b3b7acf2bf52b2f757a
kthors <kthoors@outlook.com>
2022-03-07 16:42:53 -0500

    page for forgot password

---
+++ pages/forgot.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ForgotPassword from "../components/auth/ForgotPassword";
+
+const Forgot = () => {
+  return (
+    <div className="row flex flex-center w-full bg-white dark:bg-slate-800 relative">
+      <ForgotPassword />
+    </div>
+  );
+};
+
+export default Forgot;
